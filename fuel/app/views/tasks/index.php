f18a8b66d51c215463c243b36df15d2f93a22159
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-17 11:46:33 +0900

    indexにtasks/create, tasks/detailのリンクを設置

--- fuel/app/views/tasks/index.php
+++ fuel/app/views/tasks/index.php
@@ -1,10 +1,13 @@
+<a href="<?php echo \Uri::create('/tasks/create'); ?>">新しいタスクを作成</a>
 <?php if (empty($tasks)): ?>
     <p>タスクはありません。</p>
 <?php else: ?>
     <ul>
         <?php foreach ($tasks as $task): ?>
             <li>
-                <strong><?php echo e($task['title']); ?></strong><br>
+                <a href="<?php echo \Uri::create('/tasks/detail/' . $task['id']); ?>">
+                    <strong><?php echo e($task['title']); ?></strong>
+                </a><br>
                 <?php echo nl2br(e($task['description'])); ?><br>
                 作成日: <?php echo e($task['created_at']); ?>
             </li>
