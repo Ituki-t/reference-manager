9526b1e03ad5d67d49972c5c4eb2eb0b6d7ebe3c
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-17 11:29:29 +0900

    add tasks create

--- fuel/app/classes/controller/tasks.php
+++ fuel/app/classes/controller/tasks.php
@@ -9,4 +9,22 @@ class Controller_Tasks extends Controller_Template
         $this->template->title = "タスク一覧";
         $this->template->content = \View::forge('tasks/index', array('tasks' => $tasks));
     }
+
+
+    public function action_create()
+    {
+        if (\Input::method() == 'POST') {
+            $title = \Input::post('title');
+            $description = \Input::post('description');
+            $status = \Input::post('status');
+            $dev_location = \Input::post('dev_location');
+            $deadline = \Input::post('deadline');
+            $user_id = \Session::get('user_id');
+
+            Model_Task::create_task($title, $description, $status, $dev_location, $deadline, $user_id);
+        }
+
+        $this->template->title = "タスク作成";
+        $this->template->content = \View::forge('tasks/create');
+    }
 }
--- fuel/app/classes/model/task.php
+++ fuel/app/classes/model/task.php
@@ -10,4 +10,20 @@ class Model_Task extends \Model
             ->execute()
             ->as_array();
     }
+
+    public static function create_task($title, $description, $status, $dev_location, $deadline, $user_id)
+    {
+        return \DB::insert('tasks')
+            ->set(array(
+                'title' => $title,
+                'description' => $description,
+                'status' => $status,
+                'dev_location' => $dev_location,
+                'deadline' => $deadline,
+                'user_id' => $user_id,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ))
+            ->execute();
+    }
 }
