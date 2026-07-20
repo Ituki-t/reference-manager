44557a9674183004bbe42cd08df9b61f9acb566f
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-20 18:37:13 +0900

    knockoutjsでタグの選択、削除ができるようになった

--- fuel/app/views/referenceitems/create.php
+++ fuel/app/views/referenceitems/create.php
@@ -26,11 +26,39 @@
             id="memo"
         ><?php echo Input::post('memo'); ?></textarea>
     </div>
+
+
+    /* tagの処理 */
     <div>
-        <input 
-            type="text" 
+        <label for="tag-keyword">タグ検索</label>
+        <input
+            type="text"
+            name="tag-keyword"
+            id="tag-keyword"
             data-bind="value: keyword, valueUpdate: 'input'"
         >
-        <p data-bind="text: keyword"></p> 
-    </div>
+        <p data-bind="text: keyword"></p>
+
+        <div data-bind="foreach: tags">
+            <button
+                type="button"
+                data-bind="
+                    text: name,
+                    click: $parent.selectTag
+                "
+            ></button>
+        </div>
+        <div>
+            <p>選択されたタグ:</p>
+            <div data-bind="foreach: selectedTags">
+                <span data-bind="text: name"></span>
+
+                <button 
+                    type="button" 
+                    data-bind="click: $parent.removeTag"
+                >削除
+                </button>
+            </div>
+        </div>
+    <button type="submit">作成</button>
 </form>
--- public/assets/js/referenceitems/create.js
+++ public/assets/js/referenceitems/create.js
@@ -6,6 +6,34 @@ document.addEventListener('DOMContentLoaded', function () {
         self.keyword = ko.observable('');
         self.searchResults = ko.observableArray([]);
         self.selectedTags = ko.observableArray([]);
+
+
+        // test
+        self.tags = ko.observableArray([]);
+
+        self.loadTags = function () {
+            fetch('/tags')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (tags) {
+                    console.log('Fetched tags:', tags);
+                    self.tags(tags);
+                })
+                .catch(function (error) {
+                    console.error('Error fetching tags:', error);
+                });
+        };
+        self.loadTags();
+
+        
+        self.selectTag = function (tag) {
+            self.selectedTags.push(tag);
+        };
+
+        self.removeTag = function (tag) {
+            self.selectedTags.remove(tag);
+        };
     }
 
     ko.applyBindings(new ReferenceItemViewModel());
