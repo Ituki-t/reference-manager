a50e5dd983934e35864be384875396ce3192f112
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-22 14:58:29 +0900

    template.php でbootstrapを読み込み

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo isset($title) ? e($title) : 'Reference Manager'; ?></title>
-    <?php echo Asset::js('lib/knockout-3.5.3.js'); ?>
+
+    <?php echo Asset::css('lib/bootstrap.min.css'); ?>
+
 </head>
 <body>
     <?php echo $content; ?>
+
+    <?php echo Asset::js('lib/bootstrap.bundle.min.js'); ?>
+    <?php echo Asset::js('lib/knockout-3.5.3.js'); ?>
+
 </body>
 </html>
