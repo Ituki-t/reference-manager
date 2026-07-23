325c96128c1abbb794e11f144cd04b07eb4e5408
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-24 00:34:58 +0900

    DBからreference_items_countを取得

--- fuel/app/classes/model/task.php
+++ fuel/app/classes/model/task.php
@@ -2,15 +2,21 @@
 
 class Model_Task extends \Model
 {
-    public static function get_tasks_all($user_id)
-    {
-        return \DB::select()
-            ->from('tasks')
-            ->where('user_id', '=', $user_id)
-            ->order_by('created_at', 'desc')
-            ->execute()
-            ->as_array();
-    }
+  public static function get_tasks_all($user_id)
+  {
+    return \db::select(
+      'tasks.*',
+      array(\db::expr('count(reference_items.id)'), 'reference_items_count')
+    )
+    ->from('tasks')
+    ->join('reference_items', 'left')
+    ->on('tasks.id', '=', 'reference_items.task_id')
+    ->where('tasks.user_id', '=', $user_id)
+    ->group_by('tasks.id')
+    ->order_by('created_at', 'desc')
+    ->execute()
+    ->as_array();
+  }
 
     public static function get_task_by_id($task_id, $user_id)
     {
@@ -80,17 +86,23 @@ class Model_Task extends \Model
             ->execute();
     }
 
-    public static function search_tasks_by_keyword($user_id, $keyword)
-    {
-        return \DB::select()
-            ->from('tasks')
-            ->where('user_id', '=', $user_id)
-            ->where_open()
-                ->where('title', 'like', '%' . $keyword . '%')
-                ->or_where('description', 'like', '%' . $keyword . '%')
-            ->where_close()
-            ->order_by('created_at', 'desc')
-            ->execute()
-            ->as_array();
-    }
+  public static function search_tasks_by_keyword($user_id, $keyword)
+  {
+    return \DB::select(
+      'tasks.*',
+      array(\DB::expr('count(reference_items.id)'), 'reference_items_count')
+    )
+    ->from('tasks')
+    ->join('reference_items', 'left') 
+    ->on('tasks.id', '=', 'reference_items.task_id')
+    ->where('tasks.user_id', '=', $user_id)
+    ->where_open()
+      ->where('tasks.title', 'like', '%' . $keyword . '%')
+      ->or_where('tasks.description', 'like', '%' . $keyword . '%')
+    ->where_close()
+    ->group_by('tasks.id')
+    ->order_by('tasks.created_at', 'desc')
+    ->execute()
+    ->as_array();
+  }
 }
