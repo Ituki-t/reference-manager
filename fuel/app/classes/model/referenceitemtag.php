aa1addb0fb3bf2dab8441c23053f7600754a10c8
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-25 23:53:50 +0900

    model/* のインデントを2に統一下

--- fuel/app/classes/model/referenceitemtag.php
+++ fuel/app/classes/model/referenceitemtag.php
@@ -2,21 +2,21 @@
 
 class Model_ReferenceItemTag extends \Model
 {
-    public static function add_tag_to_reference_item($reference_item_id, $tag_id)
-    {
-        \DB::insert('reference_item_tags')
-            ->set(array(
-                'reference_item_id' => $reference_item_id,
-                'tag_id' => $tag_id,
-            ))
-            ->execute();
-    }
+  public static function add_tag_to_reference_item($reference_item_id, $tag_id)
+  {
+    \DB::insert('reference_item_tags')
+      ->set(array(
+        'reference_item_id' => $reference_item_id,
+        'tag_id' => $tag_id,
+      ))
+      ->execute();
+  }
 
 
-    public static function delete_tags_by_reference_item_id($reference_item_id)
-    {
-        \DB::delete('reference_item_tags')
-            ->where('reference_item_id', $reference_item_id)
-            ->execute();
-    }
+  public static function delete_tags_by_reference_item_id($reference_item_id)
+  {
+    \DB::delete('reference_item_tags')
+      ->where('reference_item_id', $reference_item_id)
+      ->execute();
+  }
 }
