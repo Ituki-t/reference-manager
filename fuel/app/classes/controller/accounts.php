22bbcd25f7463766b6ed8b184e7052311bca261f
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-26 23:07:25 +0900

    signupの処理を分割

--- fuel/app/classes/controller/accounts.php
+++ fuel/app/classes/controller/accounts.php
@@ -2,29 +2,43 @@
 
 class Controller_Accounts extends Controller_Template
 {
-  public function action_signup()
+  private function render_signup($error = '')
   {
-    $error = "";
+    $this->template->title = "アカウント登録";
+    $this->template->content = \View::forge('accounts/signup', array(
+      'error' => $error
+    ));
+  }
 
-    if (\Input::method() === 'POST') {
-      $username = \Input::post('username');
-      $email = \Input::post('email');
-      $password = \Input::post('password');
 
-      if (empty($username) || empty($email) || empty($password)) {
-        $error = "すべての項目を入力してください。";
-      } elseif (Model_User::get_user_by_username($username)) {
-        $error = "このユーザー名は既に使用されています。";
-      } else {
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        Model_User::create_user($username, $email, $password);
+  public function get_signup()
+  {
+    $this->render_signup();
+  }
 
-        return \Response::redirect('accounts/login');
-      }
+
+  public function post_signup()
+  {
+    $username = \Input::post('username');
+    $email = \Input::post('email');
+    $password = \Input::post('password');
+
+    if (empty($username) || empty($email) || empty($password)) {
+      $error = "すべての項目を入力してください。";
+      $this->render_signup($error);
+      return;
     }
 
-    $this->template->title = "アカウント登録";
-    $this->template->content = \View::forge('accounts/signup', array('error' => $error));
+    if (Model_User::get_user_by_username($username)) {
+      $error = "このユーザー名は既に使用されています。";
+      $this->render_signup($error);
+      return;
+    }
+
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    Model_User::create_user($username, $email, $password_hash);
+
+    return \Response::redirect('accounts/login');
   }
 
 
