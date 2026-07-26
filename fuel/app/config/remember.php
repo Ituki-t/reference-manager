ec8a3540bdda90e878bf19a1e098df76070632ce
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-27 00:46:25 +0900

    confit/remember.phpのインデントを2にした

--- fuel/app/config/remember.php
+++ fuel/app/config/remember.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    // ログイン状態を保持するためのクッキーの名前
-    'cookie_name' => 'remember_token',
-    // ログイン状態を保持するためのクッキーの有効期限（秒単位）
-    'expiration' => 60 * 60 * 24 * 30, // 30 days
+  // ログイン状態を保持するためのクッキーの名前
+  'cookie_name' => 'remember_token',
+  // ログイン状態を保持するためのクッキーの有効期限（秒単位）
+  'expiration' => 60 * 60 * 24 * 30, // 30 days
 );
