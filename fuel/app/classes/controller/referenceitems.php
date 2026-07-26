96677aedb699efa8ff1c833099e8ca24b81ed123
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-26 22:01:44 +0900

    action_updateをget_, post_ render_に分割

--- fuel/app/classes/controller/referenceitems.php
+++ fuel/app/classes/controller/referenceitems.php
@@ -20,9 +20,9 @@ class Controller_ReferenceItems extends Controller_Template
 
   public function post_create($task_id)
   {
-    $title = \Input::post('title');
-    $url = \Input::post('url');
-    $memo = \Input::post('memo');
+    $title = \Input::post('title', '');
+    $url = \Input::post('url', '');
+    $memo = \Input::post('memo', '');
 
     $tag_ids = \Input::post('tag_ids', array());
 
@@ -45,11 +45,12 @@ class Controller_ReferenceItems extends Controller_Template
   public function action_detail($reference_item_id)
   {
     $reference_item = Model_ReferenceItem::get_reference_item_by_id($reference_item_id);
-    $tags = Model_Tag::get_tags_by_reference_item_id($reference_item_id);
 
     if (!$reference_item) {
       return \Response::redirect(\Uri::create('tasks'));
     }
+
+    $tags = Model_Tag::get_tags_by_reference_item_id($reference_item_id);
 
     $this->template->title = '参考資料の詳細';
     $this->template->content = \View::forge('referenceitems/detail', array(
@@ -59,40 +60,8 @@ class Controller_ReferenceItems extends Controller_Template
   }
 
 
-  public function action_update($reference_item_id)
+  private function render_update($reference_item, $tags, $error = '')
   {
-    $error = '';
-
-    $reference_item = Model_ReferenceItem::get_reference_item_by_id($reference_item_id);
-
-    if (!$reference_item) {
-      return \Response::redirect(\Uri::create('tasks'));
-    }
-
-    if (\Input::method() == 'POST') {
-      $title = \Input::post('title');
-      $url = \Input::post('url');
-      $memo = \Input::post('memo');
-
-      $tag_ids = \Input::post('tag_ids', array());
-
-      if (empty($title) || empty($url)) {
-        $error = 'タイトルとURLは必須です。';
-      } else {
-        Model_ReferenceItem::update_reference_item($reference_item_id, $title, $url, $memo);
-
-        Model_ReferenceItemTag::delete_tags_by_reference_item_id($reference_item_id);
-
-        foreach ($tag_ids as $tag_id) {
-          Model_ReferenceItemTag::add_tag_to_reference_item($reference_item_id, $tag_id);
-        }
-
-        return \Response::redirect(\Uri::create('tasks/detail/' . $reference_item['task_id']));
-      }
-    }
-
-    $tags = Model_Tag::get_tags_by_reference_item_id($reference_item_id);
-
     $this->template->title = '参考資料の更新';
     $this->template->content = \View::forge('referenceitems/update', array(
       'reference_item' => $reference_item,
@@ -102,7 +71,58 @@ class Controller_ReferenceItems extends Controller_Template
   }
 
 
-  public function action_delete($reference_item_id)
+  public function get_update($reference_item_id)
+  {
+    $reference_item = Model_ReferenceItem::get_reference_item_by_id($reference_item_id);
+
+    if (!$reference_item) {
+      return \Response::redirect(\Uri::create('tasks'));
+    }
+
+    $tags = Model_Tag::get_tags_by_reference_item_id($reference_item_id);
+
+    $this->render_update($reference_item, $tags);
+  }
+
+
+  public function post_update($reference_item_id)
+  {
+    $reference_item = Model_ReferenceItem::get_reference_item_by_id($reference_item_id);
+
+    if (!$reference_item) {
+      return \Response::redirect(\Uri::create('tasks'));
+    }
+
+    $title = \Input::post('title', '');
+    $url = \Input::post('url', '');
+    $memo = \Input::post('memo', '');
+
+    $tag_ids = \Input::post('tag_ids', array());
+
+    if (empty($title) || empty($url)) {
+      $reference_item['title'] = $title;
+      $reference_item['url'] = $url;
+      $reference_item['memo'] = $memo;
+
+      $error = 'タイトルとURLは必須です。';
+      $tags = Model_Tag::get_tags_by_reference_item_id($reference_item_id);
+      $this->render_update($reference_item, $tags, $error);
+      return;
+    }
+
+    Model_ReferenceItem::update_reference_item($reference_item_id, $title, $url, $memo);
+
+    Model_ReferenceItemTag::delete_tags_by_reference_item_id($reference_item_id);
+
+    foreach ($tag_ids as $tag_id) {
+      Model_ReferenceItemTag::add_tag_to_reference_item($reference_item_id, $tag_id);
+    }
+
+    return \Response::redirect(\Uri::create('tasks/detail/' . $reference_item['task_id']));
+  }
+
+
+  public function post_delete($reference_item_id)
   {
     $reference_item = Model_ReferenceItem::get_reference_item_by_id($reference_item_id);
 
@@ -118,12 +138,12 @@ class Controller_ReferenceItems extends Controller_Template
 
   public function action_search($task_id)
   {
-    $keyword = \input::get('keyword', '');
+    $keyword = \Input::get('keyword', '');
 
     if ($keyword === '') {
       $reference_items = Model_ReferenceItem::get_reference_items_by_task_id($task_id);
     } else {
-      $reference_items = model_ReferenceItem::get_reference_items_by_keyword($keyword, $task_id);
+      $reference_items = Model_ReferenceItem::get_reference_items_by_keyword($keyword, $task_id);
     }
 
     foreach ($reference_items as &$item) {
@@ -131,7 +151,7 @@ class Controller_ReferenceItems extends Controller_Template
     }
     unset($item);
 
-    return \response::forge(
+    return \Response::forge(
       json_encode($reference_items, JSON_UNESCAPED_UNICODE),
       200,
       array('content-type' => 'application/json')
