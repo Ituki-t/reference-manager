c5e8771852f0112cfb47c277bbf01f764d2ac4f8
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-07-29 10:01:00 +0900

    create Controller_Base

---
+++ fuel/app/classes/controller/base.php
@@ -0,0 +1,32 @@
+<?php
+
+class Controller_Base extends Controller_Template
+{
+  public function before()
+  {
+    parent::before();
+
+    if (\Session::get('user_id') !== null) {
+      return ;
+    }
+
+    // loginを保持する処理
+    \Config::load('remember', true);
+    $cookie_name = \Config::get('remember.cookie_name');
+    $token = \Cookie::get($cookie_name);
+
+    if (empty($token)) {
+      return \Response::redirect('accounts/login');
+    }
+
+    $user = Model_User::get_user_by_token($token);
+
+    if (!$user) {
+      \Cookie::delete($cookie_name);
+      return \Response::redirect('accounts/login');
+    }
+
+    \Session::set('user_id', $user['id']);
+    \Session::set('username', $user['username']);
+  }
+}
