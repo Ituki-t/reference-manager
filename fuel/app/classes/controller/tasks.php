c09a075021a2e4dcd17303b5f48c77d8e6432bec
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-08-05 18:01:59 +0900

    Revert "reference_item_idsからtagsを複数取得(n+1対策). tag一覧が取得できなくなっている"
    This reverts commit ca8b8fb921f12cc96b4a3d5995f0ab611c95742a.

--- fuel/app/classes/controller/tasks.php
+++ fuel/app/classes/controller/tasks.php
@@ -156,27 +156,10 @@ class Controller_Tasks extends Controller_Template
     // reference_items/indexの処理
     $reference_items = Model_ReferenceItem::get_reference_items_by_task_id($task_id);
 
-    $reference_item_ids = array_column($reference_items, 'id');
-
-    $tags = Model_Tag::get_tags_by_reference_item_ids($reference_item_ids);
-    $tags_by_reference_item_id = array();
-
-    foreach ($tags as $tag) {
-      $reference_item_id = $tag['reference_item_id'];
-      if (!isset($tags_by_reference_item_id[$reference_item_id])) {
-        $tags_by_reference_item_id[$reference_item_id] = array();
-      }
-      $tags_by_reference_item_id[$reference_item_id][] = array(
-        'id' => $tag['id'],
-        'name' => $tag['name']
-      );
+    foreach ($reference_items as &$item) {
+      $item['tags'] = Model_Tag::get_tags_by_reference_item_id($item['id']);
     }
-
-    foreach ($reference_items as &$reference_item) {
-      $reference_item['tags'] = $tags_by_reference_item_id[$reference_item['id']] ?? array();
-    } 
-    unset($reference_item);
-
+    unset($item);
 
     $this->template->title = "タスク詳細";
     $this->template->content = \View::forge('tasks/detail', array(
